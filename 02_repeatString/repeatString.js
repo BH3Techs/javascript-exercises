aa3496164bed85c5bda48b0dbe84e3d0596ef4bd
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-15 14:38:47 +0200

    repeat String Test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function repeatString(str,num)  {
+    let ans ='';
+    if(num>=0){
+        for(let i=0; i<num;i++){
+            ans+=str;
+        }
+    }else{
+        ans='ERROR';
+    }
+    
+    return ans;
 };
 
 // Do not edit below this line
