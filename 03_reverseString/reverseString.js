1ac3fdeb235a0d551b00e32e9384ff97c9f42454
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-15 14:48:38 +0200

    reverse String Test Pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function reverseString(str) {
+    let revStr = '';
+    for(let i = 0; i<=str.length;i++){
+        revStr += str.charAt(str.length-i)
+    }
+    return revStr;
 };
 
 // Do not edit below this line
