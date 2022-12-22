0fc1a12e10ea2891ac914597f51a98aa577d6e6b
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-22 12:30:42 +0200

    Mupliply function Corrected

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -7,19 +7,24 @@ const subtract = function(num1, num2) {
 };
 
 const sum = function(... nums) {
-  console.log("Hona  Kekutanga",nums);
   let sum =0;
   let array = nums[0];
   
 	array.forEach(element => {
     sum+=element;
   });
-  console.log('Hona', sum)
   return sum;
 };
 
-const multiply = function(num1, num2) {
-  return num1*num2;
+const multiply = function(... nums) {
+  let ans =1;
+  let array = nums[0];
+
+  array.forEach(element =>{
+    ans*=element;
+
+  });
+  return ans;
 };
 
 const power = function() {
