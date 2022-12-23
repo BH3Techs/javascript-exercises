b5605ed6dca28f939a46b07c36698872cdd529c0
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-23 20:47:52 +0200

    Acoomodated punctuation marks

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,13 @@
 const palindromes = function (string) {
+    let symbs = '';
     let array = string.split('');
+    if(array.includes('!')){
+        array = array.filter(x => !x.includes('!'));
+        symbs ='!';
+    }
     let reversedarray = array.reverse();
     let newstring = reversedarray.join('');
+    newstring +=symbs;
 
     console.log(string);
     console.log(newstring);
