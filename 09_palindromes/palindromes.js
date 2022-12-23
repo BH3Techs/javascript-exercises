652060f4d9d96438157e45810edacef5b48227d7
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-23 20:55:47 +0200

    Introduced regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,17 +1,11 @@
 const palindromes = function (string) {
-    let symbs = '';
-    let array = string.split('');
-    if(array.includes('!')){
-        array = array.filter(x => !x.includes('!'));
-        symbs ='!';
-    }
-    let reversedarray = array.reverse();
-    let newstring = reversedarray.join('');
-    newstring +=symbs;
-
-    console.log(string);
-    console.log(newstring);
-    return newstring == string;
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+  return (
+    processedString
+      .split("")
+      .reverse()
+      .join("") == processedString
+  );
 };
 
 // Do not edit below this line
