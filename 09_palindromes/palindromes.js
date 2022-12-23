511670430630d50cd2b01d81c931aadcc20e4eef
Howard Mabhugu <hmabhugu.3@gmail.com>
2022-12-23 20:33:54 +0200

    Palindrome Function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let array = string.split('');
+    let reversedarray = array.reverse();
+    let newstring = reversedarray.join('');
 
+    console.log(string);
+    console.log(newstring);
+    return newstring == string;
 };
 
 // Do not edit below this line
